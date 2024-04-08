5daac96c13cae2a23968befb0ee97242fe6973aa
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2024-04-08 11:38:51 -0700

    Added generateRandomMatrix func

--- main.go
+++ main.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"math/rand"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,15 +21,32 @@ func increments(c *gin.Context) {
 	total := 10000
 	index := 0
 
-	for index <= total {
+	var ContinueForwards bool = true
+
+	for ContinueForwards {
+		matrix := generateRandomMatrix()
 
 		c.SSEvent("index", map[string]interface{}{
-			"Index": index,
-			"Total": total,
+			"Index":  index,
+			"Total":  total,
+			"Matrix": matrix,
 		})
 		c.Writer.Flush()
 		index += 1
 	}
 
 	c.Writer.Flush()
+}
+
+func generateRandomMatrix() [][]int {
+	matrix := make([][]int, 10)
+
+	for i := 0; i < len(matrix); i++ {
+		matrix[i] = make([]int, 10)
+		for j := 0; j < len(matrix[i]); j++ {
+			matrix[i][j] = rand.Intn(100)
+		}
+	}
+
+	return matrix
 }
